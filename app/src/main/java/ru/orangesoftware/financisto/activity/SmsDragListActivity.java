7f611688514c2de24b0314361b171d58b06b7467
yetanothercoder <mbaturov@gmail.com>
2018-04-01 17:04:36 +0300

    SMS List UX Improvements #53 experimenting with simplier fragment layout

--- app/src/main/java/ru/orangesoftware/financisto/activity/SmsDragListActivity.java
+++ app/src/main/java/ru/orangesoftware/financisto/activity/SmsDragListActivity.java
@@ -2,10 +2,10 @@ package ru.orangesoftware.financisto.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -16,7 +16,7 @@ import ru.orangesoftware.financisto.db.DatabaseAdapter;
 public class SmsDragListActivity extends AppCompatActivity {
 
     protected final DatabaseAdapter db;
-
+    private RecyclerView recyclerView;
 
     public SmsDragListActivity() {
         db = new DatabaseAdapter(this);
@@ -32,12 +32,13 @@ public class SmsDragListActivity extends AppCompatActivity {
 //        }
         Toolbar menu = findViewById(R.id.tool_bar);
         setSupportActionBar(menu);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        // todo.mb: migrate init code from SmsDragListFragment
     }
     
-    private void showFragment(Fragment fragment) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.container, fragment, "fragment").commit();
-    }
+    
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
