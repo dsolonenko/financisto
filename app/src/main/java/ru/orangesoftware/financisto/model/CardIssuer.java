ce8372f237b1e0eab855112e67dda0aaae149058
Dmitry Trubin <1437843+dtruebin@users.noreply.github.com>
2018-12-15 00:32:51 +0100

    [#73] Add Mir payment system to the list of card issuers
    - Strings for most card issuers are located in common.xml, but neither
      of those are localizable. Mir originates in Russia, so there's
      a localized term for it. Hence, update strings.xml in values/ and
      values-ru/
    - Rationale for the position of the new value in CardIssuer enum: it seems
      that first the most popular payment systems are listed (with Visa- and
      Mastercard-related systems respectively grouped together), followed by
      less popular payment systems in alphabetical order -- which is why MIR
      ends up between DISCOVER and NETS.

--- app/src/main/java/ru/orangesoftware/financisto/model/CardIssuer.java
+++ app/src/main/java/ru/orangesoftware/financisto/model/CardIssuer.java
@@ -25,6 +25,7 @@ public enum CardIssuer implements EntityEnum {
 	JCB(R.string.card_issuer_jcb, R.drawable.account_type_card_jcb),
 	DINERS(R.string.card_issuer_diners, R.drawable.account_type_card_diners),
 	DISCOVER(R.string.card_issuer_discover, R.drawable.account_type_card_discover),
+	MIR(R.string.card_issuer_mir, R.drawable.account_type_card_mir),
 	NETS(R.string.card_issuer_nets, R.drawable.account_type_card_nets),
 	UNIONPAY(R.string.card_issuer_unionpay, R.drawable.account_type_card_unionpay);
 
